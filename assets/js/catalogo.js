5868358eb829617bc9964f33c7741ab6a2fc6e37
AQP20171679 <alejandraadriantejada@gmail.com>
2017-09-06 05:52:02 -0500

    Modal xbox 90

--- assets/js/catalogo.js
+++ assets/js/catalogo.js
@@ -1,32 +1,36 @@
 $(document).ready(function(){
-	$('#nint').click(modal);
-	$('#ps4').click(modal);
-	$('#xbox').click(modal);
+  $('#modal-xbox').hide();   
+  //$('#nint').click(modalN);
+  //$('#ps4').click(modal);
+  $('#xbox').click(modalX);
+  $('.btn-closeX').click(endX);
 })
 
 var arrayProveedores = [$('#nint'),$('#ps4'),$('#xbox')];
 
-function modal(e){
-	alert(arrayProveedores.indexOf(e));
+function modalX(){
+  $('#modal-xbox').show();
+  $('#modal-xbox').addClass('fondo');   
+}
+
+function endX(){
+  $('#modal-xbox').removeClass('fondo'); 
+  $('#modal-xbox').hide();
 }
 
 var draaimolen = function (){
   var albumImg = document.getElementsByClassName("img-sider-xbox");
   var teller = 0;
   var teller1 = 0;
-  var arrayImg = [];//lege array waar  foto's in moeten komen
+  var arrayImg = [];
   while (teller < albumImg.length){
     arrayImg.push(albumImg[teller].getAttribute("src"));
     teller++;
   }
-//getAttribute om elementen vast te nemen
   arrayImg.push(arrayImg.shift()); 
-// push en shift want telkens als er een foto bij komt moet er ook 1 verwijderd worden
   while (teller1 < albumImg.length){
     albumImg[teller1].setAttribute("src",arrayImg[teller1]);
-//setAttribute om de foto's toe te voegen
     teller1++;
   }
-  console.log(arrayImg.join());
 }
 setInterval(draaimolen,1000);
