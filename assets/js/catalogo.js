46d17a172eae73eb85770adfe14655475293a020
AQP20171679 <alejandraadriantejada@gmail.com>
2017-09-06 07:47:20 -0500

    Modal de 3 consolas

--- assets/js/catalogo.js
+++ assets/js/catalogo.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
-  $('#modal-xbox').hide();   
-  //$('#nint').click(modalN);
-  //$('#ps4').click(modal);
+  $('#modal-xbox').hide();
+  $('#modal-ps4').hide();   
+  $('#modal-nint').hide();   
+  $('#nint').click(modalN);
+  $('#ps4').click(modalP);
   $('#xbox').click(modalX);
   $('.btn-closeX').click(endX);
+  $('.btn-closeP').click(endP);
+  $('.btn-closeN').click(endN);
 })
 
 var arrayProveedores = [$('#nint'),$('#ps4'),$('#xbox')];
 
+/*FUNCIONES MODAL*/
 function modalX(){
   $('#modal-xbox').show();
   $('#modal-xbox').addClass('fondo');   
@@ -18,7 +23,29 @@ function endX(){
   $('#modal-xbox').hide();
 }
 
-var draaimolen = function (){
+function modalP(){
+  $('#modal-ps4').show();
+  $('#modal-ps4').addClass('fondo');   
+}
+
+function endP(){
+  $('#modal-ps4').removeClass('fondo'); 
+  $('#modal-ps4').hide();
+}
+
+function modalN(){
+  $('#modal-nint').show();
+  $('#modal-nint').addClass('fondo');   
+}
+
+function endN(){
+  $('#modal-nint').removeClass('fondo'); 
+  $('#modal-nint').hide();
+}
+
+
+
+var carruselX = function (){
   var albumImg = document.getElementsByClassName("img-sider-xbox");
   var teller = 0;
   var teller1 = 0;
@@ -33,4 +60,38 @@ var draaimolen = function (){
     teller1++;
   }
 }
-setInterval(draaimolen,1000);
+setInterval(carruselX,1000);
+
+var carruselP = function (){
+  var albumImg = document.getElementsByClassName("img-sider-ps4");
+  var teller = 0;
+  var teller1 = 0;
+  var arrayImg = [];
+  while (teller < albumImg.length){
+    arrayImg.push(albumImg[teller].getAttribute("src"));
+    teller++;
+  }
+  arrayImg.push(arrayImg.shift()); 
+  while (teller1 < albumImg.length){
+    albumImg[teller1].setAttribute("src",arrayImg[teller1]);
+    teller1++;
+  }
+}
+setInterval(carruselP,1000);
+
+var carruselN = function (){
+  var albumImg = document.getElementsByClassName("img-sider-n");
+  var teller = 0;
+  var teller1 = 0;
+  var arrayImg = [];
+  while (teller < albumImg.length){
+    arrayImg.push(albumImg[teller].getAttribute("src"));
+    teller++;
+  }
+  arrayImg.push(arrayImg.shift()); 
+  while (teller1 < albumImg.length){
+    albumImg[teller1].setAttribute("src",arrayImg[teller1]);
+    teller1++;
+  }
+}
+setInterval(carruselN,1000);
